c6dee3ce558238ef52cfae0046ca41a4d6cf686f
Andrija Petrovic <andrija.hers@gmail.com>
2019-08-23 08:05:34 -0400

    'q' lib finally Promise/A+ compliant, chai-as-promised used smoothly

--- src/mocha/globals.js
+++ src/mocha/globals.js
@@ -1,6 +1,6 @@
 var Mocha = require('mocha'),
   chai = require('chai'),
-  chap = require('allex-chai-as-promised'),
+  chap = require('chai-as-promised'),
   Path = require('path'),
   fs = require('fs');
 
